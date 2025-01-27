593bebf690045ac4dd5504d3ba2c5c82940f0066
Azrael Von Kheragüert <BenjaminMendoza@sternenzelt.net.pe>
2025-01-27 18:11:07 -0500

    a
    a

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" Content="ie-edge">
+
+        <!--Información y titulo-->
+        <title> Sternenzelt | Soluciones en Ingeniería y Software.</title>
+        <meta content="Sternenzelt es una consultora especializada en tecnología, 
+        desarrollo de software y proyectos científicos. Ofrecemos soluciones innovadoras y personalizadas 
+        para impulsar el crecimiento y la eficiencia de tu empresa. 
+        Nuestro equipo de expertos combina conocimientos en ingeniería, tecnología y ciencia para transformar tus ideas en realidades exitosas." 
+        name="description">
+        <meta content="consultoría tecnológica, desarrollo de software, proyectos científicos, ingeniería IT, soluciones tecnológicas, innovación en tecnología, Sternenzelt" name="keywords">
+        
+        <!-- Favicon -->
+        <link href="public/sternenzelt_icon.svg" type="image/svg+xml" rel="icon">
+        <!-- Final de Favicon -->
+
+        <!--Fuentes de Google-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PJLHWZJ8');</script>
+        <!-- End Google Tag Manager -->
+        
+        <?php
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, "https://api.ipquery.io/?format=json");
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+         $response = curl_exec($ch);
+         curl_close($ch);
+         echo $response;
+        ?>
+
+        <!-- =======================================================
+         * Nombre de proyecto: Sternenzelt Website
+         * Actualizado: 26/01/2025 Versión A_1.0 
+         * Author: Sternenzelt EIRL.
+         * License: https://www.sternenzelt.net.pe/licencias
+         * © 2024 Sternenzelt. Todos los derechos reservados.
+        ======================================================== -->
+    </head>
+    <body>
+        <p>a</p>
+</html>
